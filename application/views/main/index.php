bb4187788ebbc15a4b323c9e502dccd6c6c7e009
leetsxpro <leetsxpro@gmail.com>
2021-06-23 03:08:45 +0900

    20210623

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -7,10 +7,10 @@
 
 	<div class="row justify-content-center">
     <div class="col col-lg-8 px-5" style="display: flex; flex-direction: row-reverse;">
-      <div class="ms-1">
+      <div class="ms-1" style="color: #cccccc;">
 				누적 방문수: <?=$total?>
 			</div>
-			<div>
+			<div style="color: #cccccc;">
 				오늘 방문수: <?=$today?>
 			</div>
     </div>
